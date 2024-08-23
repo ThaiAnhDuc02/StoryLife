750e345b04b9ebb9d389b87f4b66e17dc5c6a703
Thái Anh Đức <140801842+ThaiAnhDuc02@users.noreply.github.com>
2024-08-23 18:20:18 +0700

    create update img to s3 but to step indentity role (#14)

--- src/routes/blogs.route.js
+++ src/routes/blogs.route.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const blogsController = require('../app/controllers/BlogsController');
+const multer = require('multer');
+const upload = multer({ storage: multer.storage });
+
 
 
 router.get('/create', blogsController.create);
-router.post('/store', blogsController.store);
+router.post('/store', upload.single('imageCover'), blogsController.store);
 router.get('/:slug', blogsController.show);
 router.get('/:id/edit', blogsController.edit);
 router.put('/:id', blogsController.update);
